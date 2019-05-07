57ab2ce9780d53c6afc993e44c5540c338943e65
jas07061002 <jas18kalra@gmail.com>
2019-05-07 19:35:41 -0400

    Creating AOP setup

---
+++ spring-demo-aop/src/com/luv2code/aop/dao/AccountDAO.java
@@ -0,0 +1,15 @@
+package com.luv2code.aop.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AccountDAO {
+	
+	public void addAccount() {
+		
+		System.out.println(getClass() + ": DOING MY DB WORK : ADDING AN ACCOUNT");
+	}
+	
+	
+
+}
