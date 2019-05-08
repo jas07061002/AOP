4f6d3d0b3088f2071f61e09546223a4913a5b0a9
jas07061002 <jas18kalra@gmail.com>
2019-05-08 17:56:07 -0400

    Joinpoint commit

---
+++ spring-demo-aop-read-joinpoint/src/com/luv2code/aop/aspect/LuvAOPExpressions.java
@@ -0,0 +1,28 @@
+package com.luv2code.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LuvAOPExpressions {
+	
+	@Pointcut("execution(* com.luv2code.aop.dao.*.*(..))")
+	public void forDAOPackage() {}
+	
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.luv2code.aop.dao.*.get*(..))")
+	public void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aop.dao.*.set*(..))")
+	public void setter() {}
+	
+	// create pointcut to include package and exclude getter/setter
+	@Pointcut("forDAOPackage() && !(getter() || setter())")
+	public void forDAOPackageNoGetterSetter() {}
+	
+	
+
+}
