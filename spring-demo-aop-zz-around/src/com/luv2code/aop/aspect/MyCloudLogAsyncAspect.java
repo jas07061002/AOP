ef3a240ab56a00fb16895d806444f388c2179f35
jas07061002 <jas18kalra@gmail.com>
2019-05-09 15:24:28 -0400

    @Around

---
+++ spring-demo-aop-zz-around/src/com/luv2code/aop/aspect/MyCloudLogAsyncAspect.java
@@ -0,0 +1,19 @@
+package com.luv2code.aop.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(1)
+public class MyCloudLogAsyncAspect {
+	
+	@Before("com.luv2code.aop.aspect.LuvAOPExpressions.forDAOPackageNoGetterSetter()")
+	public void logToCloudAsync() {
+		System.out.println("\n====>>> Logging to cloud in async fashion");
+	}
+	
+
+}
